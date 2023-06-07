549dda45b6b2530a80379d691406098d12f2105c
Mirt12 <Mirt12@yandex.ru>
2023-06-07 19:47:49 +0200

    add Test for DELETE, POST, PUT, GET requests

--- src/test/java/web/app/api/StudentTest.java
+++ src/test/java/web/app/api/StudentTest.java
@@ -6,11 +6,12 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class StudentTest {
     @Test
     public void createStudent() {
-        Student student1 = new Student(9, "Aamir", "Khan", "aamir_khan@anywhere.school");
+        Student student1 = new Student(9, "Aamirc", "Khan", "aamir_khan@anywhere.school");
         //convert java class Object to JSON as String:
         ObjectMapper mapper = new ObjectMapper();
         String studentJson = null;
@@ -28,5 +29,23 @@ public class StudentTest {
         given().baseUri(baseUrl).contentType(ContentType.JSON)
                 .when().get("/students")
                 .then().assertThat().statusCode(200).log().body();
+    }
+     @Test
+    public void testDeleteStudentById(){
+        String url = "http://localhost:8080/students/{id}";
+        int id = 2;
+        given().when().delete(url, id).then().
+                assertThat().statusCode(200).
+                body("id", equalTo(2)).
+                log().body();
+     }
+
+    @Test
+    public void testDeleteStudentByPath(){
+        String url = "http://localhost:8080/students/1";
+        given().when().delete(url).then().
+                assertThat().statusCode(200).
+                body("id", equalTo(1)).
+                log().body();
     }
 }
